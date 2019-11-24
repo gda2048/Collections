436e26b1a4603e15cf2342171e667d59068fd4b6
gda2048 <goncharovdma@gmail.com>
2019-11-24 16:22:17 +0200

    add profiles models

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -14,7 +14,7 @@ import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
+AUTH_USER_MODEL = 'profiles.User'
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'profiles.apps.ProfilesConfig',
+    'projects.apps.ProjectsConfig',
+    'reports.apps.ReportsConfig',
 ]
 
 MIDDLEWARE = [
--- backend/profiles/models.py
+++ backend/profiles/models.py
@@ -1,3 +1,48 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class User(AbstractUser):
+    about = models.CharField('О себе', max_length=1023, null=True, blank=True)
+
+    class Meta:
+        db_table = 'User'
+        verbose_name = 'User'
+        verbose_name_plural = 'Users'
+
+
+class Team(models.Model):
+    id = models.AutoField(primary_key=True)
+    team = models.ForeignKey("self", on_delete=models.CASCADE, related_name='groups', null=True, blank=True)
+    is_group = models.BooleanField('Is team?', default=True)
+    name = models.CharField('Team name', max_length=255,
+                            help_text='Team name max_length=255')
+    description = models.CharField('Team description', max_length=1023,
+                                   help_text='Team description max_length=1023', null=True, blank=True)
+    users = models.ManyToManyField(User, related_name='teams', through='Membership')
+    date_created = models.DateTimeField('Date created', default=timezone.now)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        db_table = 'teams'
+        verbose_name = 'Team'
+        verbose_name_plural = 'Teams'
+
+
+class Membership(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Member', related_name='memberships')
+    team = models.ForeignKey(Team, on_delete=models.CASCADE, verbose_name='Team', related_name='members')
+    is_manager = models.BooleanField('Is manager?', default=False)
+    date_started = models.DateTimeField('Date created', default=timezone.now)
+
+    def __str__(self):
+        return f'{self.user}:{self.team}'
+
+    class Meta:
+        db_table = 'membership'
+        unique_together = ['team', 'user']
+        verbose_name = 'Membership'
+        verbose_name_plural = 'Memberships'
